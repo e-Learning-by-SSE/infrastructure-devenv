937eb233a73a857ecf9bcd98b4dccf5e4b8a8115
spark-sse <113825305+spark-sse@users.noreply.github.com>
2024-07-07 14:02:32 +0200

    fix(bootstrap): use project name instead of volume name

--- bootstrap/bootstrap.ps1
+++ bootstrap/bootstrap.ps1
@@ -1,62 +1,53 @@
 param (
-    [switch]$delete,
-    [string]$file = "compose-dev.yml",
-    [string]$repo,
-    [string]$volume
+    [switch]$Delete,
+    [string]$ComposeFile = "compose-dev.yml",
+    [string]$Repo,
+    [string]$Project
 )
 
-# Funktion zur Anzeige der Nutzungshinweise
-function Show-Usage {
-    Write-Host "Usage: bootstrap.ps1 [-d|--delete] [-f|--file compose-file] -r|--repo <GitHub Repository URL> -v|--volume <Volume Name>"
-    Write-Host "  -d, --delete         Delete the volume before cloning"
-    Write-Host "  -f, --file           Specify the Docker Compose file (default: compose-dev.yml)"
-    Write-Host "  -r, --repo           Specify the GitHub Repository URL"
-    Write-Host "  -v, --volume         Specify the Docker Volume Name"
+function Usage {
+    Write-Host "Usage: $($MyInvocation.MyCommand.Name) [-Delete] [-ComposeFile <Compose File>] -Repo <GitHub Repository URL> -Project <Project Name>"
+    Write-Host "  -Delete          Delete the volume before cloning"
+    Write-Host "  -ComposeFile     Specify the Docker Compose file (default: compose-dev.yml)"
+    Write-Host "  -Repo            Specify the GitHub Repository URL"
+    Write-Host "  -Project         Specify the Project Name"
     exit 1
 }
 
-# Standardwerte setzen
-$DELETE_VOLUME = $false
-$COMPOSE_FILE = $file
-$REPO_URL = $repo
-$VOLUME_NAME = $volume
-
-# Überprüfen der Optionen
-if ($delete) {
-    $DELETE_VOLUME = $true
+# Validate required parameters
+if (-not $Repo -or -not $Project) {
+    Usage
 }
 
-# Überprüfen, ob die erforderlichen Argumente übergeben wurden
-if ([string]::IsNullOrEmpty($REPO_URL) -or [string]::IsNullOrEmpty($VOLUME_NAME)) {
-    Show-Usage
+# Set volume name based on project name
+$VolumeName = "${Project}-repository"
+
+# Delete the volume if the -Delete option is set
+if ($Delete) {
+    Write-Host "Deleting volume $VolumeName..."
+    docker volume rm $VolumeName -f
+    Write-Host "Proceeding after attempting to delete volume $VolumeName."
 }
 
-# Löschen des Volumes, falls die Option -d gesetzt wurde
-if ($DELETE_VOLUME) {
-    Write-Host "Deleting volume $VOLUME_NAME..."
-    if (-not (docker volume rm $VOLUME_NAME)) {
-        Write-Host "Failed to delete volume $VOLUME_NAME."
-        exit 1
-    }
-    Write-Host "Volume $VOLUME_NAME deleted successfully."
-}
+# Check if the volume already exists
+$VolumeExists = docker volume ls --format '{{.Name}}' |  Where-Object { $_ -eq $VolumeName }
 
-# Überprüfen, ob das Volume bereits existiert
-$VOLUME_EXISTS = docker volume ls --format '{{.Name}}' | Where-Object { $_ -eq $VOLUME_NAME }
-
-if (-not $VOLUME_EXISTS) {
-    # Docker-Container verwenden, um das Repository in das Volume zu klonen
-    Write-Host "Cloning repository $REPO_URL into volume $VOLUME_NAME..."
-    if (-not (docker run --rm -v ${VOLUME_NAME}:/devenv docker git clone $REPO_URL /devenv)) {
+if (-not $VolumeExists) {
+    # Use a Docker container to clone the repository into the volume
+    Write-Host "Cloning repository $Repo into volume $VolumeName..."
+    if (-not (docker run --rm -it -v "${VolumeName}:/${Project}" docker git clone $Repo "/${Project}")) {
         Write-Host "Failed to clone repository."
         exit 1
     }
-    Write-Host "Repository cloned into volume $VOLUME_NAME successfully."
+    Write-Host "Repository cloned into volume $VolumeName successfully."
+
+    # Set permissions to 1000 for the cloned repository
+    Write-Host "Setting permissions for ${Project} to 1000..."
+    docker run --rm -v "${VolumeName}:/${Project}" docker chown -R 1000:1000 "/${Project}"
 } else {
-    Write-Host "Volume $VOLUME_NAME already exists. Skipping clone."
+    Write-Host "Volume $VolumeName already exists. Skipping clone."
 }
 
-# Docker Compose ausführen, wobei das geklonte Repository als Volume verwendet wird
-Write-Host "Starting Docker Compose with $COMPOSE_FILE..."
-docker run --rm -e VOLUME_NAME=$VOLUME_NAME -v ${VOLUME_NAME}:/devenv -v /var/run/docker.sock:/var/run/docker.sock -w /devenv docker compose  -f ${COMPOSE_FILE} up
-
+# Run Docker Compose using the cloned repository as a volume
+Write-Host "Starting Docker Compose with $ComposeFile..."
+docker run --rm -e VOLUME_NAME=$VolumeName -v "${VolumeName}:/${Project}" -v "/var/run/docker.sock:/var/run/docker.sock" -w "/${Project}" docker compose -f $ComposeFile up
