b18c01fc6a8fff82cf5fafb1b6f44e90bd4e4610
spark-sse <113825305+spark-sse@users.noreply.github.com>
2024-09-05 16:00:52 +0200

    feat(javascript): installed docker i

---
+++ javascript/install-docker.sh
@@ -0,0 +1,5 @@
+apt-get install lsb-release ca-certificates apt-transport-https software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
